1a4a6163deb18351dc85c6d7ed062d7074de8c25
Ivani <katarzyna.stosio@gmail.com>
2019-08-23 21:31:46 +0200

    created space for adding a new book

--- addNewBook.js
+++ addNewBook.js
@@ -1,3 +1,17 @@
 function addNewBookHandler() {
     console.log('Dodaje pozycje')
 }
+
+var tableCollection = document.getElementsByTagName('table')[0]
+
+document.createElement
+
+const tableCollection1 = document.getElementsByTagName('table');
+const tableHandler = tableCollection1[0]
+
+const tr = document.createElement('tr')
+const td = document.createElement('td')
+td.textContent = 'Kasia';
+
+tr.append(td)
+tableHandler.append(tr);
