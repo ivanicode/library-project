b310f18d92588ceb077930c41bb6b479c22b450a
Ivani <katarzyna.stosio@gmail.com>
2019-08-26 22:05:24 +0200

    working on add new book button

--- addNewBook.js
+++ addNewBook.js
@@ -1,17 +1,26 @@
 function addNewBookHandler() {
-    console.log('Dodaje pozycje')
-}
 
-var tableCollection = document.getElementsByTagName('table')[0]
-
-document.createElement
+//document.createElement
 
 const tableCollection1 = document.getElementsByTagName('table');
-const tableHandler = tableCollection1[0]
+const tableHandler = tableCollection1[0];
 
-const tr = document.createElement('tr')
-const td = document.createElement('td')
-td.textContent = 'Kasia';
+const tr = document.createElement('tr');
+const td = document.createElement('td');
+const inputAuthor = document.createElement('input');
+inputAuthor.setAttribute('placeholder','author');
+const inputTitle = document.createElement('input');
+inputTitle.setAttribute('placeholder','title');
+const tdDate = document.createElement('td');
+tdDate.textContent = '26.08.2019';
 
-tr.append(td)
-tableHandler.append(tr);
+//td.append(inputAuthor);
+//td.append(inputTitle);
+tr.append(tdDate);
+tr.append(td);
+tableHandler.prepend(tr);
+
+
+
+}
+
