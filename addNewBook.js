0f7516d82ed63e8002a6d8cd8e97757457ed93c4
Ivani <katarzyna.stosio@gmail.com>
2019-08-28 18:52:41 +0200

    added working version of select

--- addNewBook.js
+++ addNewBook.js
@@ -6,19 +6,42 @@ const tableCollection1 = document.getElementsByTagName('table');
 const tableHandler = tableCollection1[0];
 
 const tr = document.createElement('tr');
-const td = document.createElement('td');
-const inputAuthor = document.createElement('input');
-inputAuthor.setAttribute('placeholder','author');
-const inputTitle = document.createElement('input');
-inputTitle.setAttribute('placeholder','title');
+
 const tdDate = document.createElement('td');
 tdDate.textContent = '26.08.2019';
 
-//td.append(inputAuthor);
-//td.append(inputTitle);
 tr.append(tdDate);
-tr.append(td);
+
+const tdTitle = document.createElement('td');
+const inputTitle = document.createElement('input');
+inputTitle.setAttribute('placeholder','title');
+
+tdTitle.append(inputTitle);
+tr.append(tdTitle);
+
+const tdAuthor = document.createElement('td');
+const inputAuthor = document.createElement('input');
+inputAuthor.setAttribute('placeholder','author');
+
+tdAuthor.append(inputAuthor);
+tr.append(tdAuthor);
+
+const tdCategory = document.createElement('td');
+const selectCategory = document.createElement('select');
+selectCategory.setAttribute('multiple', 'true');
+
+const optionRomance = document.createElement('option');
+optionRomance.setAttribute('value', 'romance');
+optionRomance.textContent = 'Romans';
+
+selectCategory.append(optionRomance);
+tdCategory.append(selectCategory);
+tr.append(tdCategory);
+
+
+
 tableHandler.prepend(tr);
+
 
 
 
